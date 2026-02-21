c8717cb19593365019f41c5b1c1bfe8f5b839fa3
supakornrai <supakorn.rai@ku.th>
2026-02-21 19:08:07 +0700

    add staff and add feature selected role to admin

---
+++ lib/features/staff/add_car_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddCarView extends StatelessWidget{
+  const AddCarView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+body: Center(
+  child: Text("Add Car"),
+),
+
+    );
+    
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:the_rentz/features/auth/auth_page.dart';
 import 'package:the_rentz/features/customer/customer_layout.dart';
 import 'firebase_options.dart';
+import 'package:the_rentz/features/staff/staff_layout.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:the_rentz/features/admin/admin_layout.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,25 +15,55 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Scaffold(
+        builder: (context, authSnapshot) {
+          if (authSnapshot.connectionState == ConnectionState.waiting) {
+            return  Scaffold(
               body: Center(child: CircularProgressIndicator()),
             );
           }
 
-          if (snapshot.hasData) {
-            return CustomerLayout();
-          } else {
+          if (!authSnapshot.hasData) {
             return AuthPage();
           }
+
+          final user = authSnapshot.data!;
+
+          return FutureBuilder<DocumentSnapshot>(
+            future: FirebaseFirestore.instance
+                .collection('users')
+                .doc(user.uid)
+                .get(),
+            builder: (context, roleSnapshot) {
+              if (roleSnapshot.connectionState == ConnectionState.waiting) {
+                return  Scaffold(
+                  body: Center(child: CircularProgressIndicator()),
+                );
+              }
+
+              if (!roleSnapshot.hasData || !roleSnapshot.data!.exists) {
+                return  Scaffold(
+                  body: Center(child: Text("User data not found")),
+                );
+              }
+
+              final role = roleSnapshot.data!['role'];
+
+              if (role == 'admin') {
+                return AdminLayout();
+              } else if (role == 'staff') {
+                return StaffLayout();
+              } else {
+                return CustomerLayout();
+              }
+            },
+          );
         },
       ),
     );
